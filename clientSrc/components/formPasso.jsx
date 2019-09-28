9ff94cfe84dede26853c82f6561dcd150fd516c0
Fabiano Nascimento <fabianorodrigo@gmail.com>
2019-09-28 14:33:47 -0300

    Inclusão de Autosuggest para incluir passo do tipo "executar outro procedimento"

--- clientSrc/components/formPasso.jsx
+++ clientSrc/components/formPasso.jsx
@@ -1,43 +1,72 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import InputLabel from "@material-ui/core/InputLabel";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Autosuggest from 'react-autosuggest';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import deburr from 'lodash/deburr';
+import match from 'autosuggest-highlight/match';
+import parse from 'autosuggest-highlight/parse';
 
 const useStyles = makeStyles(theme => ({
   container: {
-    display: "flex",
-    flexWrap: "wrap"
+    display: 'flex',
+    flexWrap: 'wrap',
   },
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
-    minWidth: '350px'
+    minWidth: '350px',
   },
   dialogPaper: {
-    minHeight: "350px",
-    maxHeight: "450px"
-  }
+    minHeight: '350px',
+    maxHeight: '450px',
+  },
+  suggestionsContainerOpen: {
+    position: 'absolute',
+    zIndex: 1,
+    marginTop: theme.spacing.unit,
+    left: 0,
+    right: 0,
+  },
+  suggestion: {
+    display: 'block',
+  },
+  suggestionsList: {
+    margin: 0,
+    padding: 0,
+    listStyleType: 'none',
+  },
+  divider: {
+    height: theme.spacing.unit * 2,
+  },
 }));
 
 export default function formPasso(props) {
   const classes = useStyles();
+  let timeout = React.useRef(null);
 
-  const [tipo, setTipo] = React.useState("descricao");
+  const [tipo, setTipo] = React.useState('descricao');
   const [passo, setPasso] = React.useState({
-    idPapel: "",
-    descricao: "",
-    executarId: ""
+    idPapel: '',
+    descricao: '',
+    executarId: '',
   });
+  const [textoAutosuggest, setTextoAutosuggest] = React.useState('');
+  const [
+    listaProcessosAutosuggest,
+    setListaProcessosAutosuggest,
+  ] = React.useState([]);
 
   const handleSalvar = () => {
     props.onSalvar(passo);
@@ -51,7 +80,22 @@ export default function formPasso(props) {
     setPasso({ ...passo, [name]: value });
   };
 
-  const papeis = props.app.getState("Papeis");
+  const papeis = props.app.getState('Papeis');
+  const autosuggestProps = {
+    renderInputComponent,
+    suggestions: listaProcessosAutosuggest,
+    onSuggestionsFetchRequested: handleSuggestionsFetchRequested,
+    onSuggestionsClearRequested: handleSuggestionsClearRequested,
+    onSuggestionSelected: (event, suggestionSelection) => {
+      setPasso({
+        ...passo,
+        executarId: suggestionSelection.suggestion,
+      });
+    },
+    getSuggestionValue,
+    renderSuggestion,
+  };
+
   return (
     <Dialog
       open={props.app.getState(props.formName) || false}
@@ -62,7 +106,12 @@ export default function formPasso(props) {
     >
       <DialogTitle id="form-dialog-title">Passo</DialogTitle>
       <DialogContent>
-        <FormControl variant="outlined" className={classes.textField} margin="normal" fullWidth>
+        <FormControl
+          variant="outlined"
+          className={classes.textField}
+          margin="normal"
+          fullWidth
+        >
           <InputLabel autoFocus htmlFor="outlined-age-simple">
             Papel
           </InputLabel>
@@ -71,8 +120,8 @@ export default function formPasso(props) {
             value={passo.idPapel}
             onChange={onChange}
             inputProps={{
-              name: "idPapel",
-              id: "outlined-age-simple"
+              name: 'idPapel',
+              id: 'outlined-age-simple',
             }}
             labelWidth={100}
           >
@@ -82,14 +131,22 @@ export default function formPasso(props) {
             {papeis &&
               Object.values(papeis).map(papel => {
                 return (
-                  <MenuItem key={`menuItem${papel.id}${papel.titulo}`} value={papel.id}>
+                  <MenuItem
+                    key={`menuItem${papel.id}${papel.titulo}`}
+                    value={papel.id}
+                  >
                     {papel.titulo}
                   </MenuItem>
                 );
               })}
           </Select>
         </FormControl>
-        <FormControl variant="outlined" className={classes.textField} margin="normal" fullWidth>
+        <FormControl
+          variant="outlined"
+          className={classes.textField}
+          margin="normal"
+          fullWidth
+        >
           <InputLabel htmlFor="outlined-age-simple">Tipo Descrição</InputLabel>
           <Select
             label="Tipo"
@@ -98,26 +155,54 @@ export default function formPasso(props) {
               setTipo(e.target.value);
             }}
             inputProps={{
-              name: "tipo",
-              id: "outlined-age-simple"
+              name: 'tipo',
+              id: 'outlined-age-simple',
             }}
             labelWidth={100}
           >
             <MenuItem value="descricao">Descrição textual</MenuItem>
-            <MenuItem value="executarId">Executar outro procedimento registrado</MenuItem>
+            <MenuItem value="executarId">
+              Executar outro procedimento registrado
+            </MenuItem>
           </Select>
         </FormControl>
-        <TextField
-          label="Descrição"
-          onChange={onChange}
-          id="descricao"
-          name={tipo}
-          className={classes.textField}
-          margin="normal"
-          variant="outlined"
-          fullWidth
-          multiline={true}
-        />
+        {tipo === 'descricao' && (
+          <TextField
+            label="Descrição"
+            onChange={onChange}
+            id="descricao"
+            name={tipo}
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+            fullWidth
+            multiline={true}
+          />
+        )}
+        {tipo === 'executarId' && (
+          <Autosuggest
+            {...autosuggestProps}
+            inputProps={{
+              classes,
+              placeholder: 'Informe o procedimento que será executado',
+              value: textoAutosuggest,
+              onChange: (event, { newValue }) => {
+                setTextoAutosuggest(newValue);
+              },
+            }}
+            theme={{
+              container: classes.container,
+              suggestionsContainerOpen: classes.suggestionsContainerOpen,
+              suggestionsList: classes.suggestionsList,
+              suggestion: classes.suggestion,
+            }}
+            renderSuggestionsContainer={options => (
+              <Paper {...options.containerProps} square>
+                {options.children}
+              </Paper>
+            )}
+          />
+        )}
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose} color="primary">
@@ -129,4 +214,103 @@ export default function formPasso(props) {
       </DialogActions>
     </Dialog>
   );
+  function renderInputComponent(inputProps) {
+    const { classes, inputRef = () => {}, ref, ...other } = inputProps;
+
+    return (
+      <TextField
+        label="Procedimento"
+        className={classes.textField}
+        margin="normal"
+        variant="outlined"
+        fullWidth
+        InputProps={{
+          inputRef: node => {
+            ref(node);
+            inputRef(node);
+          },
+          classes: {
+            input: classes.input,
+          },
+        }}
+        {...other}
+      />
+    );
+  }
+
+  function renderSuggestion(suggestion, { query, isHighlighted }) {
+    const matches = match(suggestion.titulo, query);
+    const parts = parse(suggestion.titulo, matches);
+    return (
+      <MenuItem selected={isHighlighted} component="div">
+        <div>
+          {parts.map((part, index) =>
+            part.highlight ? (
+              <span key={String(index)} style={{ fontWeight: 500 }}>
+                {part.text}
+              </span>
+            ) : (
+              <strong key={String(index)} style={{ fontWeight: 300 }}>
+                {part.text}
+              </strong>
+            ),
+          )}
+        </div>
+      </MenuItem>
+    );
+  }
+
+  function buscaSugestoesProcedimentos(value) {
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
+    if (value.trim() != '') {
+      timeout.current = setTimeout(async () => {
+        const procedimentosSugeridos = await props.app.controllers.Procedimento.buscaProcessosPorTitulo(
+          {
+            textoFiltro: value,
+          },
+        );
+        setListaProcessosAutosuggest(procedimentosSugeridos);
+      }, 300);
+    } else {
+      props.app.controllers.Procedimento.limpaProcessos();
+    }
+
+    /*const inputValue = deburr(value.trim()).toLowerCase();
+    const inputLength = inputValue.length;
+    let count = 0;
+
+    return inputLength === 0
+      ? []
+      : Object.values(
+          props.app.controllers.Procedimento.buscaProcessos({
+            textoFiltro: value,
+            grupoIdFiltro: '',
+          }),
+        ); listaProcessosAutosuggest.filter(suggestion => {
+          const keep =
+            count < 5 &&
+            suggestion.descricao.slice(0, inputLength).toLowerCase() ===
+              inputValue;
+
+          if (keep) {
+            count += 1;
+          }
+
+          return keep;
+        });*/
+  }
+
+  function getSuggestionValue(suggestion) {
+    return suggestion.titulo;
+  }
+
+  function handleSuggestionsFetchRequested({ value }) {
+    buscaSugestoesProcedimentos(value);
+  }
+
+  function handleSuggestionsClearRequested() {
+    setListaProcessosAutosuggest([]);
+  }
 }
--- clientSrc/controllers/ProdecimentoController.js
+++ clientSrc/controllers/ProdecimentoController.js
@@ -1,5 +1,5 @@
 //serviços
-import { Service } from "../lib/service";
+import { Service } from '../lib/service';
 
 class ProcedimentoController {
   constructor(appController) {
@@ -7,42 +7,84 @@ class ProcedimentoController {
   }
 
   limpaProcessos() {
-    this.app.setState("resultadoBuscaProcedimentos", {});
+    this.app.setState('resultadoBuscaProcedimentos', {});
   }
 
-  async buscaProcessos({ textoFiltro, grupoIdFiltro }) {
-    this.app.setState("loading", true);
+  /**
+   * Busca no conjunto de procedimentos aqueles que tenham a ocorrência de {textoFiltro} no campo titulo e retorna
+   * @param {string} textoFiltro Texto que será procurado no titulo
+   */
+  async buscaProcessosPorTitulo({ textoFiltro }) {
+    this.app.setState('loading', true);
     try {
       const response = await Service.getAllLike({
-        nomeModeloPlural: "Procedimentos",
+        nomeModeloPlural: 'Procedimentos',
         //FIXME: Por alguma razão que não descobri, o loopback-connector-mongodb não está conseguindo filtrar
         //pelo idGrupo mesmo quando coloco somente ele via "localhost:3000/explorer/processos"
         //filtroAND: grupoIdFiltro != "" && grupoIdFiltro != null? [{idGrupo:grupoIdFiltro}]:[],
         expressaoFiltro: textoFiltro,
-        campos: ["titulo", "entradas", "saidas", "passos.descricao"]
+        campos: ['titulo'],
+      });
+      const retorno = [];
+      response.data.forEach(async p => {
+        retorno.push({
+          id: p.id,
+          titulo: p.titulo,
+          grupo: this.app.getState('Grupos')[p.idGrupo],
+        });
+      });
+      return retorno;
+    } catch (e) {
+      console.error(`Exceção capturada em 'buscaProcessosPorTitulo'`, e);
+      app.mostraMensagem(`Falha ao buscar ${nomeEntidadePlural}`, 'error');
+    } finally {
+      this.app.setState('loading', false);
+    }
+  }
+
+  /**
+   * Busca no conjunto de procedimentos aqueles que tenham a ocorrência de {textoFiltro} nos campos titulo, entradas, saidas
+   * ou passos.descricao e atualiza a state global 'resultadoBuscaProcedimentos'
+   * @param {string} textoFiltro Texto que será procurado nos campos
+   * @param {string} grupoIdFiltro Se setado, tratará somente processos que estejam neste grupo
+   */
+  async buscaProcessos({ textoFiltro, grupoIdFiltro }) {
+    this.app.setState('loading', true);
+    try {
+      const response = await Service.getAllLike({
+        nomeModeloPlural: 'Procedimentos',
+        //FIXME: Por alguma razão que não descobri, o loopback-connector-mongodb não está conseguindo filtrar
+        //pelo idGrupo mesmo quando coloco somente ele via "localhost:3000/explorer/processos"
+        //filtroAND: grupoIdFiltro != "" && grupoIdFiltro != null? [{idGrupo:grupoIdFiltro}]:[],
+        expressaoFiltro: textoFiltro,
+        campos: ['titulo', 'entradas', 'saidas', 'passos.descricao'],
       });
       //Agrupando itens por grupo
       let grupoItens = {};
       response.data.forEach(async p => {
         //FIXME: Por alguma razão que não descobri, o loopback-connector-mongodb não está conseguindo filtrar
         //pelo idGrupo mesmo quando coloco somente ele via "localhost:3000/explorer/processos"
-        if (grupoIdFiltro == null || grupoIdFiltro.trim() == "" || grupoIdFiltro === p.idGrupo) {
+        if (
+          grupoIdFiltro == null ||
+          grupoIdFiltro.trim() == '' ||
+          grupoIdFiltro === p.idGrupo
+        ) {
           if (grupoItens[p.idGrupo] == null) {
             grupoItens[p.idGrupo] = {
               id: p.idGrupo,
-              titulo: this.app.getState("Grupos")[p.idGrupo].titulo,
-              processos: []
+              titulo: this.app.getState('Grupos')[p.idGrupo].titulo,
+              processos: [],
             };
           }
           grupoItens[p.idGrupo].processos.push(p);
         }
       });
-      this.app.setState("resultadoBuscaProcedimentos", grupoItens);
+      this.app.setState('resultadoBuscaProcedimentos', grupoItens);
     } catch (e) {
       console.error(`Exceção capturada em 'buscaProcessos'`, e);
-      app.mostraMensagem(`Falha ao buscar ${nomeEntidadePlural}`, "error");
+      app.mostraMensagem(`Falha ao buscar ${nomeEntidadePlural}`, 'error');
     } finally {
-      this.app.setState("loading", false);
+      this.app.setState('loading', false);
     }
   }
 }
