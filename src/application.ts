8064ed606d272f83c7d8d7e6043a541dcb85bc0b
Fabiano Nascimento <fabianorodrigo@gmail.com>
2021-01-08 17:26:39 -0300

    Configuração do datasource Mongo a partir das variáveis de ambiente

--- src/application.ts
+++ src/application.ts
@@ -1,42 +1,48 @@
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig} from '@loopback/core';
-import {
-  RestExplorerBindings,
-  RestExplorerComponent,
-} from '@loopback/rest-explorer';
+import {RestExplorerBindings, RestExplorerComponent} from '@loopback/rest-explorer';
 import {RepositoryMixin} from '@loopback/repository';
 import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import * as path from 'path';
 import {MySequence} from './sequence';
 
-export class PassoAPassoAppApplication extends BootMixin(
-  ServiceMixin(RepositoryMixin(RestApplication)),
-) {
-  constructor(options: ApplicationConfig = {}) {
-    super(options);
+export class PassoAPassoAppApplication extends BootMixin(ServiceMixin(RepositoryMixin(RestApplication))) {
+    constructor(options: ApplicationConfig = {}) {
+        super(options);
 
-    // Set up the custom sequence
-    this.sequence(MySequence);
+        //Setando o datasource com dados de variáveis de ambiente
+        //TL;DR: if you are using @loopback/boot to load your datasources (as is the default in LB4 applications
+        //scaffolded using lb4 CLI tool), then you can bind just the custom datasource configuration.
+        this.bind('datasources.config.passoApassoDS').to({
+            name: 'passoApassoDS',
+            connector: 'mongodb',
+            host: process.env.MONGO_HOST || 'Variável de ambiente MONGO_HOST sem valor',
+            port: process.env.MONGO_PORT || 'Variável de ambiente MONGO_PORT sem valor',
+            database: process.env.MONGO_DATABASE || 'Variável de ambiente MONGO_DATABASE sem valor',
+        });
 
-    // Set up default home page
-    this.static('/', path.join(__dirname, '../public'));
+        // Set up the custom sequence
+        this.sequence(MySequence);
 
-    // Customize @loopback/rest-explorer configuration here
-    this.bind(RestExplorerBindings.CONFIG).to({
-      path: '/explorer',
-    });
-    this.component(RestExplorerComponent);
+        // Set up default home page
+        this.static('/', path.join(__dirname, '../public'));
 
-    this.projectRoot = __dirname;
-    // Customize @loopback/boot Booter Conventions here
-    this.bootOptions = {
-      controllers: {
-        // Customize ControllerBooter Conventions here
-        dirs: ['controllers'],
-        extensions: ['.controller.js'],
-        nested: true,
-      },
-    };
-  }
+        // Customize @loopback/rest-explorer configuration here
+        this.bind(RestExplorerBindings.CONFIG).to({
+            path: '/explorer',
+        });
+        this.component(RestExplorerComponent);
+
+        this.projectRoot = __dirname;
+        // Customize @loopback/boot Booter Conventions here
+        this.bootOptions = {
+            controllers: {
+                // Customize ControllerBooter Conventions here
+                dirs: ['controllers'],
+                extensions: ['.controller.js'],
+                nested: true,
+            },
+        };
+    }
 }
